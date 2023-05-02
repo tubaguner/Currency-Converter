29c03a3300b53752217eb908b11423f3657b0590
Your GitHub username <Your GitHub email>
2023-05-02 17:47:47 +0300

    I improved the styling.

--- currency.py
+++ currency.py
@@ -1,9 +1,13 @@
 import tkinter as Tk
 from tkinter import ttk
 
+
 root = Tk.Tk()
 root.title('Currency Converter')
 root.geometry("520x300")
+
+style = ttk.Style()
+style.configure('Pink.TButton', background='#ff007f',foreground='white',font=('Arial Bold',16))
 
 
 def calculate_currency_value(): 
@@ -13,12 +17,10 @@ def calculate_currency_value():
     TL_var.set(f"Exchange: {TL:.2f}")
     
     
-
-
 calculate_currency = ttk.Frame(root)
 calculate_currency.pack(padx=10, pady=10, fill='x', expand=True)
 
-dollar_label = ttk.Label(calculate_currency,text="Dollar:")
+dollar_label = ttk.Label(calculate_currency,text="Dollar:",font=('Arial Bold',16))
 dollar_label.pack(fill='x', expand=True)
 
 dollar_var = Tk.StringVar()
@@ -26,24 +28,22 @@ dollar_entry = ttk.Entry(calculate_currency,textvariable=dollar_var)
 dollar_entry.pack(fill='x', expand=True)
 dollar_entry.focus()
 
-# TL
-#TL_label = ttk.Label(calculate_currency,text="TL:")
-#TL_label.pack(fill='x', expand=True)
 
-#TL_var = Tk.StringVar()
-#TL_entry = ttk.Entry(calculate_currency,textvariable=TL_var)
-#TL_entry.pack(fill='x', expand=True)
-
-TL_label = ttk.Label(calculate_currency,text="TL:")
+TL_label = ttk.Label(calculate_currency,text="TL:",font=('Arial Bold' ,16))
 TL_label.pack(fill='x', expand=True)
 
 TL_var = Tk.StringVar()
 TL_entry = ttk.Entry(calculate_currency,textvariable=TL_var)
 TL_entry.pack(fill='x', expand=True)
 
-convert_button = ttk.Button(calculate_currency,text="Convert",command=calculate_currency_value)
-convert_button.pack(fill='y', expand=False, pady=10)
+convert_button = ttk.Button(calculate_currency,text="Convert",style='Pink.TButton',command=calculate_currency_value)
+convert_button.pack(fill='y', expand=False, pady=6)
 
+style = ttk.Style()
+style.configure('TLabel', background='#ffb6c1')
+
+
+root.configure(bg='#ff69b4')
 
 
 root.mainloop()
